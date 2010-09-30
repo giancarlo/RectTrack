e7d59dee97f082cf7dbea2423181c115b2ee9726
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2010-09-30 18:07:40 -0400

    Added Program.cs

---
+++ RectTrackTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RectTrackTest
+{
+    static class Program
+    {
+        static Form1 mainform;
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            mainform = new Form1();
+            Application.Run(mainform);
+        }
+    }
+}
